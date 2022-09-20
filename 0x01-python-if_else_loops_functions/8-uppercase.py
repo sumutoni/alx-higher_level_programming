977650f814ceec48c9b4dd8bade094e97151edf1
sumutoni <steumutoni@gmail.com>
2022-09-20 16:30:09 -0700

    function that prints uppercasestring

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
+    strU = ""
     for i in range(0, len(str)):
         asc = ord(str[i])
         asc = asc - 32
-        str = chr(asc) + str[(i + 1):]
-    print("{}".format(str))
+        strU = strU + chr(asc)
+    print("{}".format(strU))
