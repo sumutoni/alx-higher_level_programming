48bd6db8572e83f161b5f5a9c81560a46303da15
sumutoni <steumutoni@gmail.com>
2022-09-20 16:34:12 -0700

    function that prints uppercasestring

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,11 @@ def uppercase(str):
     strU = ""
     for i in range(0, len(str)):
         asc = ord(str[i])
-        asc = asc - 32
-        strU = strU + chr(asc)
+        if asc >= 65 and asc <= 90:
+            strU = strU + str[i]
+        elif asc >= 97 and asc <= 122:
+            asc = asc - 32
+            strU = strU + chr(asc)
+        else:
+            strU = strU + str[i]
     print("{}".format(strU))
