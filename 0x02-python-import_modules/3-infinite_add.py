de714be711aaa43c05c0a05b24bca0525b67538e
sumutoni <steumutoni@gmail.com>
2022-09-22 10:24:08 -0700

    program to add arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-sum = 0
-for i in range(1, len(sys.argv)):
-    sum += int(sys.argv[i])
-print("{:d}".format(sum))
+if __name__ == "__main__":
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+    print("{:d}".format(sum))
