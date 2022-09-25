7adab7e68ef4138f9d4325e113cb474471c75ff2
sumutoni <steumutoni@gmail.com>
2022-09-25 04:49:27 -0700

    a function that prints all integers of a list.

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def print_list_integer(myList=[]):
+    for x in myList:
+        print("{:d}".format(x))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+This directory is for python programs working with lists and tuples
