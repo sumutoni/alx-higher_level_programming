e331c4b264f1e2bdf56e8a2620c0870e8f6885ba
sumutoni <steumutoni@gmail.com>
2022-10-10 13:36:29 -0700

    a function that prints x elements of a list with try/except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
-            count+=1
+            count += 1
         print("")
         return count
     except IndexError:
