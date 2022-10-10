76c2814c5709c4361702c9fd42409b31906471a1
sumutoni <steumutoni@gmail.com>
2022-10-10 15:21:15 -0700

    a function that raises a name exception with a message.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
-import sys
 #!/usr/bin/python3
-def safe_print_integer(value):
+import sys
+
+
+def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as ex:
-        print("Exception: " + ex, file = sys.stderr)
+        print("Exception: " + ex, file=sys.stderr)
         return False
