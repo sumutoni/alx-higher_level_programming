bbf780be13a192cb5532cc798464c938d15fabca
sumutoni <steumutoni@gmail.com>
2022-10-12 08:39:08 -0700

    Square class with getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A class Square with a private instance attribute size"""
+
+
+class Square:
+    """defining Square class and validating size value"""
+    def __init__(self, size=0):
+        self.size(size)
+    @property
+    def size(self):
+        """Getter Method"""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """Setter Method"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """A function to calculate area of square"""
+        return self.__size ** 2
