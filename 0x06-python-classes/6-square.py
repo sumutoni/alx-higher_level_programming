a2b58970056916d778774ba698a3861b9b0dedb5
sumutoni <steumutoni@gmail.com>
2022-10-12 11:49:35 -0700

    Square class with private instance attribute position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""A class Square with a private instance attribute size"""
+
+
+class Square:
+    """defining Square class and validating size value"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter Method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter Method"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) != int and type(value) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """A function to calculate area of square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+            return
+        for h in range(0, self.__position[1]):
+            print("")
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                for k in range(0, self.__position[0]):
+                    print(" ", end="")
+                print("#", end="")
+            print()
