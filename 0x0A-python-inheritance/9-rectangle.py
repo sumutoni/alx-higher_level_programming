a696a37be4dc1d5e5434aa1a59f26e1099e02cd2
sumutoni <steumutoni@gmail.com>
2022-10-24 13:27:50 -0700

    implemented area and added __Str__

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,4 +19,4 @@ class Rectangle(BaseGeometry):
 
     def __str__():
         """prints rectangle"""
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return ("[Rectangle] {w}/{h}".format(w=self.__width, h=self.__height))
