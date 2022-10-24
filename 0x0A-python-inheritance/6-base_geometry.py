9e8ef2f05621bc986894e684908edd0e08ddef77
sumutoni <steumutoni@gmail.com>
2022-10-24 12:16:55 -0700

    added area method to class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Made additions to BaseGeomtry class"""
+
+
+class BaseGeometry():
+    """Improved class"""
+
+    def __init__(self):
+        """initialization method"""
+        pass
+
+    def area(self):
+        """calculate area"""
+        raise Exception("area() is not implemented")
