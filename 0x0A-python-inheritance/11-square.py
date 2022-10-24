04d2de9eda17a50709fcecfe95d48c9f10e7c012
sumutoni <steumutoni@gmail.com>
2022-10-24 14:45:22 -0700

    added __str__

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """Class inheriting from Rectangle"""
     def __init__(self, size):
         """constructor"""
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
