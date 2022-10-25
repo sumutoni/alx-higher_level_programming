fd2dfad6ae6182b05158ffab6e5b73bdbb2695b9
sumutoni <steumutoni@gmail.com>
2022-10-25 13:25:50 -0700

    Student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defining class Student with instance attributes\
+    first_name, last_name, and age"""
+
+
+class Student:
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """return dictionary description of object"""
+        return (self.__dict__.copy())
