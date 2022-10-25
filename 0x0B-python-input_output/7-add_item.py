bb81caebf3ee228480a3d9e1e3122594426c7574
sumutoni <steumutoni@gmail.com>
2022-10-25 13:06:40 -0700

    takes arguments and saves them to JSON file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,12 +7,14 @@ save = __import__('5-save_to_json_file').save_to_json_file
 
 def add_item(args=[]):
     """adds arguments to list and saves them to JSON file"""
-    try: 
+    try:
         fil = "add_item.json"
         elements = load(fil)
     except FileNotFoundError:
         elements = []
     new_list = elements + args
     save(new_list, fil)
+
+
 if __name__ == "__main__":
     add_item(sys.argv[1:])
