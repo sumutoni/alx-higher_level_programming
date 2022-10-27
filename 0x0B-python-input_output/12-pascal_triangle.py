bc5a41368942a35a1f5cb854a0ed0623b6572ba2
sumutoni <steumutoni@gmail.com>
2022-10-27 05:58:34 -0700

    function to print pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,6 +6,8 @@ def pascal_triangle(n):
     """return a list of lists of integers representing\
         pascal's triangle"""
     trgle = []
+    if n <= 0:
+        return trgle
     trgle.append([1])
     for i in range(n - 1):
         new = []
