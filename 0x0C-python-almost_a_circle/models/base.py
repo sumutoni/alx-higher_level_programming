9afe90e54e47178cf06977cae4a4cd3fc1f1b244
sumutoni <steumutoni@gmail.com>
2022-11-03 10:21:15 -0700

    corrected coding style issues

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,10 @@
 class Base:
     """Base class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """check if id is not None before assigning it value"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
