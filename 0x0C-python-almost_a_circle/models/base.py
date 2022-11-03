660081b3db7996df43fbd92409deb6da78b00f7a
sumutoni <steumutoni@gmail.com>
2022-11-03 10:19:31 -0700

    created base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,3 +6,9 @@ class Base:
     """Base class"""
     __nb_objects = 0
     def __init__(self, id=None):
+        """check if id is not None before assigning it value"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
