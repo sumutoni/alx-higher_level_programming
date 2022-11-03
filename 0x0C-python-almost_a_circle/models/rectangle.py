2e4febce0743c15ba2b07793b2fdb432f427a4ac
sumutoni <steumutoni@gmail.com>
2022-11-03 12:44:38 -0700

    improved display method by including x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,7 +76,11 @@ class Rectangle(Base):
 
     def display(self):
         """displays rectangle using # character"""
+        for li in range(self.__y):
+            print()
         for i in range(self.__height):
+            for s in range(self.__x):
+                print(" ", end='')
             for j in range(self.__width):
                 print("#", end='')
             print()
