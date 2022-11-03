c6ba54ea172a2a49825260d94f0be33df090f723
sumutoni <steumutoni@gmail.com>
2022-11-03 11:19:29 -0700

    updated error messages

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -37,35 +37,35 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """setter method"""
-        if value <= 0:
-            raise ValueError("width must be > 0")
         if type(value) != int:
             raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
         """setter method"""
-        if value <= 0:
-            raise ValueError("height must be > 0")
         if type(value) != int:
             raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @x.setter
     def x(self, value):
         """setter method"""
-        if value < 0:
-            raise ValueError("x must be >= 0")
         if type(value) != int:
             raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
         """setter method"""
-        if value < 0:
-            raise ValueError("y must be >= 0")
         if type(value) != int:
             raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
