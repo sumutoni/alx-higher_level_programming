f6bbdc6752a42fd7f7f452417979ee30dda65e12
sumutoni <steumutoni@gmail.com>
2022-11-04 11:37:51 -0700

    added json function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """The base class for other classes that will be created later"""
+import json
 
 
 class Base:
@@ -13,3 +14,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
