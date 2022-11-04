52f833df9e1387605753257b03f9fd8e27717fca
sumutoni <steumutoni@gmail.com>
2022-11-04 11:38:58 -0700

    added json function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """returns JSON string representation of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
