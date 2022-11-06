2ab806fde14caa5046801249c1bf1b2ad56e6fb7
sumutoni <steumutoni@gmail.com>
2022-11-06 02:31:13 -0800

    added new tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -40,16 +40,37 @@ class TestRectangle(unittest.TestCase):
      def test_valEr(self):
          with self.assertRaises(ValueError):
              self.r5 = Rectangle(2, 0)
+
+     def test_valEr2(self):
+         with self.assertRaises(ValueError):
              self.r6 = Rectangle(2, 1, -4)
+
+     def test_valEr3(self):
+         with self.assertRaises(ValueError):
              self.r7 = Rectangle(2, 1, 4, -3)
+
+     def test_valEr4(self):
+         with self.assertRaises(ValueError):
              self.r12 = Rectangle(-9, 3)
+
+     def test_valEr5(self):
+         with self.assertRaises(ValueError):
              self.r13 = Rectangle(-7, 9)
 
      def test_typeError(self):
          with self.assertRaises(TypeError):
              self.r8 = Rectangle("2", 7)
+
+     def test_typeEr(self):
+         with self.assertRaises(TypeError):
              self.r9 = Rectangle(2, "7")
+
+     def test_typeEr2(self):
+         with self.assertRaises(TypeError):
              self.r10 = Rectangle(2, 7, "8")
+
+     def test_typeEr3(self):
+         with self.assertRaises(TypeError):
              self.r11 = Rectangle(2, 7, 8, "9")
 
      def test_area(self):
