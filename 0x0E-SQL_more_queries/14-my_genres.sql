8138d63d1403ab5f459ddaf68019564e693408c9
sumutoni <steumutoni@gmail.com>
2022-11-30 12:55:06 -0800

    lists all genres of the show Dexter

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- lists all genres of the show Dexter
+SELECT DISTINCT tv_genres.`name`
+FROM tv_show_genres
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.`title` = 'Dexter'
+ORDER BY tv_genres.`name` ASC;
