daa02ff84dabdd4e473bea24cdbf5097729ef66f
sumutoni <steumutoni@gmail.com>
2022-11-30 10:47:47 -0800

    create table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create database and table in database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa(
+CREATE TABLE IF NOT EXISTS states(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	name VARCHAR(256) NOT NULL
 );
