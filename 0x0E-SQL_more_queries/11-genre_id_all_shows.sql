da4d1996fbec9cdeb5f37667f030f5339f62f1f1
sumutoni <steumutoni@gmail.com>
2022-11-30 12:06:05 -0800

    lists all shows contained in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- list all shows contained in the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_show_genre.genre_id = tv_shows.show_id
+LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_shows.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
