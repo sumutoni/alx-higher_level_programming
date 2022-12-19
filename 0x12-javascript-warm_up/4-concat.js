47d7e8aaa4e8a761ce197f4451e3b7193ee3a357
sumutoni <steumutoni@gmail.com>
2022-12-19 11:12:28 -0800

    printing script

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const process = require('process');
-arg1 = process.argv[2];
-arg2 = process.argv[3];
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 console.log(arg1 + ' is ' + arg2);
