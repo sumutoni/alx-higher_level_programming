13053746d4b72bc68a4f95ef25e1a2314d18c88c
sumutoni <steumutoni@gmail.com>
2022-12-19 12:01:27 -0800

    script to print using loop and array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const ar = ['C is fun', 'Pyhton is cool', 'JavaScript is amazing'];
+const ar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < ar.length; i++) {
   console.log(ar[i]);
 }
