ac603a0cb8fbb52e05207bc6419be7695d9baa32
sumutoni <steumutoni@gmail.com>
2022-12-20 10:47:49 -0800

    added a print function

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) super.print();
+    else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(`${c}`.repeat(this.width));
+      }
+    }
+  }
+};
