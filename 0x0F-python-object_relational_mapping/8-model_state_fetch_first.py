78f6eacba6425bec4d8f61615baec8dca8799d85
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-01-04 19:45:37 +0200

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                            sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_al(engine)
+    Base.metadata.create_all(engine)
     session = Session(engine)
     state = session.query(State).order_by(State.id).first()
     if state:
