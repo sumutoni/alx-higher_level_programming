f65e96c3e9a57aa0ccdfc84e9578e77771c727a9
sumutoni <steumutoni@gmail.com>
2023-01-04 05:21:50 -0800

    retrieve all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     db = mdb.connect(host="localhost", port=3306, user=sys.argv[1],
                      passwd=sys.argv[2], db=sys.argv[3])
-    curs = db.Cursor()
+    curs = db.cursor()
     curs.execute("""SELECT * FROM states ORDER BY id ASC;""")
     for state in curs.fetchall():
         print(state)
