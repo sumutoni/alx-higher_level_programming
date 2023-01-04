7ff33d6910fc223f4652207eb38a41195fde8857
sumutoni <steumutoni@gmail.com>
2023-01-04 05:17:15 -0800

    retrieve all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import MySQLdb as mdb
+import sys
+""" Import all states from a database"""
+
+if __name__ == "__main__":
+    db = mdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3])
+    curs = db.Cursor()
+    curs.execute("""SELECT * FROM states ORDER BY id ASC;""")
+    for state in curs.fetchall():
+        print(state)
+    curs.close()
+    db.close()
