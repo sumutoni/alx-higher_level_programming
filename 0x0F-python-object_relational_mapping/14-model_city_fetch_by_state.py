2f82a6e2954ce20cf70c6016dceabfaee07ce6e4
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-01-04 21:51:41 +0200

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,6 +2,7 @@
 """Retrieve all States using ORM"""
 
 from model_state import Base, State
+from model_city import City
 from sqlalchemy import create_engine
 import sys
 from sqlalchemy.orm import sessionmaker
