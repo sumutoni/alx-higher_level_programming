b8dd9d124bfe3b4a74002cbd15ce348146849260
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-01-04 16:11:37 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,12 +8,8 @@ if __name__ == "__main__":
     db = mdb.connect(host="localhost", port=3306, user=sys.argv[1],
                      passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
-    arg = sys.argv[4].split(';')
-    st = ''
-    st = arg[0].split('"')
-    st = arg[0].split("'")
     curs.execute("""SELECT * FROM states WHERE
-                 name = '{}' ORDER BY id ASC;""".format(st))
+                 name = %s ORDER BY id ASC;""", (sys.argv[4],))
     for state in curs.fetchall():
         print(state)
     curs.close()
