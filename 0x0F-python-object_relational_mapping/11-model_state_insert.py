7ec4c4278f61cc3604335f5878a1578b47af8fdc
sumutoni <steumutoni@gmail.com>
2023-01-04 10:50:30 -0800

    add an object

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Retrieve all States using ORM"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]), echo=False)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(State(name='Louisiana'))
+    session.commit()
