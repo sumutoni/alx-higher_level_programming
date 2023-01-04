19f2506e3bbdffc4574263b2d040ca3a0ad92ed1
sumutoni <steumutoni@gmail.com>
2023-01-04 05:45:13 -0800

    match the required state name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                      passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
     curs.execute("""SELECT * FROM states WHERE
-                 name = {} ORDER BY id ASC;""".format(sys.argv[4]))
+                 name = '{}' ORDER BY id ASC;""".format(sys.argv[4]))
     for state in curs.fetchall():
         print(state)
     curs.close()
