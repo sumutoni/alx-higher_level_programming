009a446750317c2a78fed22f39dc131426d7917f
sumutoni <steumutoni@gmail.com>
2023-01-04 09:31:37 -0800

    fetch first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Retrieve all States using ORM"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_al(engine)
+    session = Session(engine)
+    states = session.query(State).order_by(State.id)
+    if len(states) == 0:
+        print('Nothing')
+    else:
+        print(states[0].__str__())
