692065d1f442ed6f5965530311139d431f582258
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-01-04 22:17:18 +0200

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -14,7 +14,7 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
     def __str__(self):
         """Print function"""
