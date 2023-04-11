7a9179b6b09e9a68df371a4e4a55dba08621abb7
sumutoni <steumutoni@gmail.com>
2023-04-11 07:55:33 -0700

    script that prints 3 lines but by using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const proc = require('process');
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < array.length) {
+	console.log(array[i]);
+	i = i + 1;
+}
