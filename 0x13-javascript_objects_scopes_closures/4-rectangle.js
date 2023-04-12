aca993aa66f5a0941250dccefb54f8c706a7c274
sumutoni <steumutoni@gmail.com>
2023-04-12 07:25:32 -0700

    added 2 more methods to interchange h and w and to double both h and w

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+
+  rotate () {
+    const i = this.width;
+    this.width = this.height;
+    this.height = i;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
