024a1562f974428c6724a17d0b03ed19f633c546
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-12 18:50:45 +0200

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -10,7 +10,7 @@ fs.readFile(process.argv[2], 'utf8', (err, data) => {
 });
 fs.readFile(process.argv[3], 'utf8', (err, data) => {
   if (err) throw err;
-  fs.writeFile(process.argv[4], data, (err) => {
+  fs.appendFile(process.argv[4], data, (err) => {
     if (err) throw err;
   });
 });
