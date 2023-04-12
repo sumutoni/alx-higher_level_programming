2f22e6429e6b68ba4dd9333fe3d2c6f03b27d1c3
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-12 16:49:31 +0200

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
+const Square = require('./5-square');
+module.exports = class Square extends Square {
   constructor (size) {
-    super(size, size);
+    super(size);
   }
 
   charPrint (c) {
