e883d909374419513c0e14633cb616f32ab4c905
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-12 18:49:04 +0200

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,15 +4,13 @@ let data1 = '';
 let data2 = '';
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) throw err;
-  console.log(data);
+  fs.writeFile(process.argv[4], data, (err) => {
+    if (err) throw err;
+  });
 });
 fs.readFile(process.argv[3], 'utf8', (err, data) => {
   if (err) throw err;
-  console.log(data);
-});
-fs.writeFile(process.argv[4], data1, (err) => {
-  if (err) throw err;
-});
-fs.writeFile(process.argv[4], data2, (err) => {
-  if (err) throw err;
+  fs.writeFile(process.argv[4], data, (err) => {
+    if (err) throw err;
+  });
 });
