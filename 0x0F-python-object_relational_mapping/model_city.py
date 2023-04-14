1f3b7b0b83489f73f28d727ca66d22946a18521a
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-15 00:16:05 +0200

    changed state_id to not null

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -12,7 +12,7 @@ class City(Base):
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
     def __str__(self):
         """Printiing object"""
