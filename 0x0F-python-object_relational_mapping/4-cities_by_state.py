1610aaf3c879de78738b3525ea9c0a4ad63bbca3
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-14 14:45:58 +0200

    Update 4-cities_by_state.py
    Added ORDER BY condition

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
                      passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
     command = """SELECT cities.id, cities.name, states.name FROM cities
-                 INNER JOIN states ON cities.state_id = states.id"""
+                 INNER JOIN states ON cities.state_id = states.id
+                 ORDER BY cities.id ASC"""
     curs.execute(command)
     for city in curs.fetchall():
         print(city)
