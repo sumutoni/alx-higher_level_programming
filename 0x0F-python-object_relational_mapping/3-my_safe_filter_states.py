4c6db5de0390d139f7b9a33cbea8acbed0e49b1a
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-14 14:30:03 +0200

    Update 3-my_safe_filter_states.py
    testing other injection cases

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,10 @@ if __name__ == "__main__":
     db = mdb.connect(host="localhost", port=3306, user=sys.argv[1],
                      passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
-    state = sys.argv[4].split()[0] + " " + sys.argv[4].split()[1]
+    if ' ' in sys.argv[4]:
+        state = sys.argv[4].split()[0] + " " + sys.argv[4].split()[1]
+    else:
+        state = sys.argv[4].split()[0]        
     curs.execute("""SELECT * FROM states WHERE name LIKE BINARY '{}'
                  ORDER BY id ASC""".format(state))
     for st in curs.fetchall():
