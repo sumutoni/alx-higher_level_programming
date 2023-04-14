752326ed5485b9fb24e4becb57f30546ac570f3d
sumutoni <steumutoni@gmail.com>
2023-04-14 15:14:36 -0700

    City class

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""City class that links to cities table"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Class definition"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
+
+    def __str__(self):
+        """Printiing object"""
+        return "({}) {}".format(self.id, self.name)
