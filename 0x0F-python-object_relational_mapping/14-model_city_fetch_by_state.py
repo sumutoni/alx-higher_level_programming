d0b8c2bfe8b68be10cff688d7011f0491ca4f348
sumutoni <54947406+sumutoni@users.noreply.github.com>
2023-04-15 00:30:20 +0200

    Changed printing statement

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
     session = Session()
     for state, city in session.query(State, City).filter(
             State.id == City.state_id).order_by(City.id):
-        print(state.name + ": " + city)
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
