36725b194d94b1f218885c84d4a38605f4d2c848
sumutoni <steumutoni@gmail.com>
2023-04-14 15:13:58 -0700

    print all cities and their states

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script that list all cities objects from database"""
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state, city in session.query(State, City).filter(
+            State.id == City.state_id)order_by(City.id):
+        print(state.name + " " + city)
+    session.close()
