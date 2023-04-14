ac05d3b40ab47473cef115f5560d03386c8f456a
sumutoni <steumutoni@gmail.com>
2023-04-14 04:30:59 -0700

    python script that prints all states in a db

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Script to print all states from database"""
+
+import sys
+import MySQLdb as mdb
+
+
+if __name__ == "__main__":
+    db = mdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3])
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states ORDER BY id")
+    for state in curs.fetchall():
+        print(state)
+    curs.close()
+    db.close()
