46c955ec501c77b0c40d3cd844af62b2fa786079
sumutoni <steumutoni@gmail.com>
2023-04-14 04:49:47 -0700

    get all states starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script to print all states starting with 'N'"""
+
+import sys
+import MySQLdb as mdb
+
+
+if __name__ == "__main__":
+    db = mdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3])
+    curs = db.cursor()
+    curs.execute("""SELECT * FROM states WHERE name LIKE 'N%'
+                 ORDER BY id ASC;""")
+    for state in curs.fetchall():
+        print(state)
+    curs.close()
+    db.close()
