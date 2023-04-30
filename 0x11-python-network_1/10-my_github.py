bad23bc79df2489ea33ce95db25bc66b8de2c7e1
sumutoni <steumutoni@gmail.com>
2023-04-30 16:46:48 -0700

    changed api to ensure user authenetication

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,7 +5,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/users/{}'.format(argv[1])
+    url = 'https://api.github.com/user'
     headers = {'Accept': 'application/vnd.github+json'}
     r = requests.get(url, headers=headers, auth=(argv[1], argv[2]))
     print(r.json().get('id'))
