4f1c171fc83d2d5e3c94dbe6129bc089834e5f55
sumutoni <steumutoni@gmail.com>
2023-04-30 16:41:28 -0700

    get github user id using token and username

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that sends post request to URL with letter as parameter"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    headers = {'Accept': 'application/vnd.github+json'}
+    r = requests.get(url, headers=headers, auth=(argv[1], argv[2]))
+    print(r.json().get('id'))
