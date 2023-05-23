d22099ef948736eb81ef67ec066f7400c80c13ca
sumutoni <steumutoni@gmail.com>
2023-05-23 15:45:55 -0700

    get film title using api

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const url ='https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 console.log(url);
-request(url, function (response, body) {
-  console.log(JSON.parse(body).title);
+request(url, function (err, response, body) {
+  if (!err) {
+    console.log(JSON.parse(body).title);
+  }
 });
