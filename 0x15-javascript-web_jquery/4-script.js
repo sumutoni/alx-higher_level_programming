8f2307706ce96e8b598ad0ca2ad67060ac7534dc
sumutoni <steumutoni@gmail.com>
2023-05-31 10:22:11 -0700

    toggle class of header

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+$('DIV#toggle_header').on('click', function (event) {
+  if ($('header').hasClass('red')) {
+    $('header').removeClass('red');
+    $('header').addClass('green');
+  } else if ($('header').hasClass('green')) {
+    $('header').removeClass('green');
+    $('header').addClass('red');
+  } else {
+    $('header').addClass('red');
+  }
+});
